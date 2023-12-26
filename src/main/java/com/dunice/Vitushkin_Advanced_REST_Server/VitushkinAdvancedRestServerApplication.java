b5de14c7d101522c7023206d99c10f1b0dbcf396
DVitushkin <d.vitushkin@mk.dunice.net>
2023-12-26 15:52:37 +0300

    Add entry point for server

---
+++ src/main/java/com/dunice/Vitushkin_Advanced_REST_Server/VitushkinAdvancedRestServerApplication.java
@@ -0,0 +1,13 @@
+package com.dunice.Vitushkin_Advanced_REST_Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitushkinAdvancedRestServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitushkinAdvancedRestServerApplication.class, args);
+	}
+
+}
