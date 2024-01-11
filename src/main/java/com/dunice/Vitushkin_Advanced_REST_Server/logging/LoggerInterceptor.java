87516a79fdc64e77bf661de0ce6c648c4c8ec2eb
DVitushkin <d.vitushkin@mk.dunice.net>
2024-01-11 14:57:45 +0300

    Add logging to database

--- src/main/java/com/dunice/Vitushkin_Advanced_REST_Server/logging/LoggerInterceptor.java
+++ src/main/java/com/dunice/Vitushkin_Advanced_REST_Server/logging/LoggerInterceptor.java
@@ -5,31 +5,10 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @Component
 public class LoggerInterceptor implements HandlerInterceptor {
-
-    @Override
-    public boolean preHandle(
-            HttpServletRequest request,
-            HttpServletResponse response,
-            Object handler
-    ) throws Exception {
-        log.info("[preHandle][" + request + "]" + "[" + request.getMethod()
-                + "]" + request.getRequestURI() + request.getParameterMap());
-        return true;
-    }
-
-    @Override
-    public void postHandle(
-            HttpServletRequest request,
-            HttpServletResponse response,
-            Object handler,
-            ModelAndView modelAndView) throws Exception {
-        log.info("[postHandle][" + request + "]");
-    }
 
     @Override
     public void afterCompletion(
