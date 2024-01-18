885966d87938311042ee29388da8b726a9150d91
DVitushkin <d.vitushkin@mk.dunice.net>
2024-01-18 12:36:03 +0300

    Replace validation annotation in put user dto class To more excplicity

--- src/main/java/com/dunice/Vitushkin_Advanced_REST_Server/dto/user/PutUserDto.java
+++ src/main/java/com/dunice/Vitushkin_Advanced_REST_Server/dto/user/PutUserDto.java
@@ -9,19 +9,19 @@ import lombok.Data;
 
 @Data
 public class PutUserDto {
-    @NotNull(message = ErrorsMsg.USER_AVATAR_NOT_NULL)
+    @NotBlank(message = ErrorsMsg.USER_AVATAR_NOT_NULL)
     private String avatar;
 
     @Email(message = ErrorsMsg.USER_EMAIL_NOT_VALID, regexp = "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
-    @NotNull(message = ErrorsMsg.USER_EMAIL_NOT_NULL)
+    @NotBlank(message = ErrorsMsg.USER_EMAIL_NOT_NULL)
     @Size(min = 3, max = 100, message = ErrorsMsg.EMAIL_SIZE_NOT_VALID)
     private String email;
 
-    @NotNull(message = ErrorsMsg.USER_NAME_HAS_TO_BE_PRESENT)
+    @NotBlank(message = ErrorsMsg.USER_NAME_HAS_TO_BE_PRESENT)
     @Size(min = 3, max = 25, message = ErrorsMsg.USERNAME_SIZE_NOT_VALID)
     private String name;
 
-    @NotNull(message = ErrorsMsg.USER_ROLE_NOT_NULL)
+    @NotBlank(message = ErrorsMsg.USER_ROLE_NOT_NULL)
     @Size(min = 3, max = 100, message = ErrorsMsg.UNKNOWN)
     private String role;
 }
