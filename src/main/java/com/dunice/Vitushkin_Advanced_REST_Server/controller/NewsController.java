e7685bb342646d03e0c852e34ecdb857260411d2
DVitushkin <d.vitushkin@mk.dunice.net>
2024-01-19 10:52:47 +0300

    Add get paginated news handler and validation errors constant

--- src/main/java/com/dunice/Vitushkin_Advanced_REST_Server/controller/NewsController.java
+++ src/main/java/com/dunice/Vitushkin_Advanced_REST_Server/controller/NewsController.java
@@ -1,15 +1,19 @@
 package com.dunice.Vitushkin_Advanced_REST_Server.controller;
 
+import com.dunice.Vitushkin_Advanced_REST_Server.dto.news.GetNewsOutDto;
 import com.dunice.Vitushkin_Advanced_REST_Server.dto.news.NewsDto;
+import com.dunice.Vitushkin_Advanced_REST_Server.exception.ErrorsMsg;
 import com.dunice.Vitushkin_Advanced_REST_Server.response.CreateNewsSuccessResponse;
+import com.dunice.Vitushkin_Advanced_REST_Server.response.PageableResponse;
 import com.dunice.Vitushkin_Advanced_REST_Server.service.NewsService;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/news")
@@ -22,5 +26,18 @@ public class NewsController {
             @Validated @RequestBody NewsDto request
     ) {
         return ResponseEntity.ok(newsService.createUser(request));
+    }
+
+    @GetMapping
+    public ResponseEntity<PageableResponse<List<GetNewsOutDto>>> getPaginatedNews(
+            @RequestParam("page")
+            @Min(value = 1, message = ErrorsMsg.PAGE_SIZE_NOT_VALID)
+            Integer page,
+            @RequestParam("perPage")
+            @Min(value = 1, message = ErrorsMsg.PER_PAGE_MIN_NOT_VALID)
+            @Max(value = 100, message = ErrorsMsg.PER_PAGE_MAX_NOT_VALID)
+            Integer perPage
+    ) {
+        return ResponseEntity.ok(newsService.getPaginatedNews(page, perPage));
     }
 }
