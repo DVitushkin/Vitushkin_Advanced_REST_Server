1a0faeceb72bb8653c7ac0ff9820a0527c06409a
DVitushkin <d.vitushkin@mk.dunice.net>
2024-01-25 16:42:15 +0300

    Add count using tags method in news repository

--- src/main/java/com/dunice/Vitushkin_Advanced_REST_Server/repository/NewsRepository.java
+++ src/main/java/com/dunice/Vitushkin_Advanced_REST_Server/repository/NewsRepository.java
@@ -1,9 +1,11 @@
 package com.dunice.Vitushkin_Advanced_REST_Server.repository;
 
+import java.util.List;
 import java.util.UUID;
 
 import com.dunice.Vitushkin_Advanced_REST_Server.models.News;
 
+import com.dunice.Vitushkin_Advanced_REST_Server.models.Tag;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -15,6 +17,8 @@ import org.springframework.data.jpa.repository.Query;
 public interface NewsRepository extends JpaRepository<News, Long>, JpaSpecificationExecutor<News> {
     @Query("select n from News n join fetch n.tags where n.user.id = :userId")
     Page<News> findAllByUserId(UUID userId, PageRequest of);
+
+    long countAllByTags(Tag tag);
 
     Page<News> findAll(Specification<News> spec, Pageable pageable);
 }
