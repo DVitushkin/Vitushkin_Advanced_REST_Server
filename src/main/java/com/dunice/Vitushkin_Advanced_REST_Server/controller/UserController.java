5f9e54bbf396832a9b4297a31525de850027c750
DVitushkin <d.vitushkin@mk.dunice.net>
2024-01-29 11:27:15 +0300

    Change argument Principal in controllers

--- src/main/java/com/dunice/Vitushkin_Advanced_REST_Server/controller/UserController.java
+++ src/main/java/com/dunice/Vitushkin_Advanced_REST_Server/controller/UserController.java
@@ -1,20 +1,21 @@
 package com.dunice.Vitushkin_Advanced_REST_Server.controller;
 
+import java.security.Principal;
+import java.util.List;
+import java.util.UUID;
+
 import com.dunice.Vitushkin_Advanced_REST_Server.dto.user.PutUserDto;
 import com.dunice.Vitushkin_Advanced_REST_Server.exception.ErrorsMsg;
-import com.dunice.Vitushkin_Advanced_REST_Server.models.User;
 import com.dunice.Vitushkin_Advanced_REST_Server.response.BaseSuccessResponse;
 import com.dunice.Vitushkin_Advanced_REST_Server.response.CustomSuccessResponse;
 import com.dunice.Vitushkin_Advanced_REST_Server.response.PutUserDtoResponse;
 import com.dunice.Vitushkin_Advanced_REST_Server.service.user.UserService;
 import com.dunice.Vitushkin_Advanced_REST_Server.views.PublicUserView;
 import jakarta.validation.Valid;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-
 import jakarta.validation.constraints.Positive;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,9 +23,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/v1/user")
@@ -38,8 +36,8 @@ public class UserController {
     }
 
     @GetMapping("/info")
-    public ResponseEntity<CustomSuccessResponse<PublicUserView>> getUserInfo(@AuthenticationPrincipal User user) {
-        return ResponseEntity.ok(userService.getUserInfo(user));
+    public ResponseEntity<CustomSuccessResponse<PublicUserView>> getUserInfo(Principal principal) {
+        return ResponseEntity.ok(userService.getUserInfo(principal));
     }
 
     @GetMapping("/{id}")
@@ -51,14 +49,14 @@ public class UserController {
 
     @PutMapping
     public ResponseEntity<CustomSuccessResponse<PutUserDtoResponse>> putUserinfo(
-            @AuthenticationPrincipal User user,
+            Principal principal,
             @Valid @RequestBody PutUserDto request
     ) {
-        return ResponseEntity.ok(userService.putUserInfo(user, request));
+        return ResponseEntity.ok(userService.putUserInfo(principal, request));
     }
 
     @DeleteMapping
-    public ResponseEntity<BaseSuccessResponse> deleteUser(@AuthenticationPrincipal User user) {
-        return ResponseEntity.ok(userService.deleteUser(user));
+    public ResponseEntity<BaseSuccessResponse> deleteUser(Principal principal) {
+        return ResponseEntity.ok(userService.deleteUser(principal));
     }
 }
